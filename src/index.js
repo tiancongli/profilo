fbc1684b4e559b4b1133c3027fc24b0fb4130530
tiancong li <litiancong@hotmail.com>
2019-05-08 12:08:16 +1000

    1.change to yarn\n2.make the outline

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Scene } from './components/card.js';
+import { Row, Col } from 'antd'
+import './index.scss';
+
+function MainList(props) {
+  const numbers = [0, 1, 2, 3, 4, 5];
+  return (
+    <Row>
+      {numbers.map((number) => 
+        <Col
+          md={8} sm={12} xs={24}
+        >
+          <Scene/>
+        </Col>)}
+    </Row>
+  );
+}
 
 ReactDOM.render(
-  <Scene/>,
+  <div>
+    <header>
+      <div>
+      <h1 className="title DEATHNOTEFont">
+        Welcome to TL's World
+      </h1>
+      <h2>
+        and have fun.
+      </h2>
+      </div>
+    </header>
+    <main>
+      <div className="spearhead">
+        <div className="skills">
+          skills
+        </div>
+        <div className="intro">
+          self intro
+        </div>
+      </div>
+      <MainList/>
+    </main>
+    <footer>
+      <div>
+        ICY Design ©2019 Created by TL
+      </div>
+    </footer>
+  </div>,
   document.getElementById('root')
 );
